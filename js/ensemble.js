9a432a0a40ca93bbce05336b9fc686c79de8a2c5
Felipe Fabel <felipecamposfabel@gmail.com>
2015-03-05 17:22:25 -0300

    Frases Refeitas, Bug Corrigido, Adicionados Contatos.

--- js/ensemble.js
+++ js/ensemble.js
@@ -1,3 +1,26 @@
 $(document).ready(function () {
+    if($('.main-container').height() < $(window).height()){
+        $('.bubbles-container').css('height',$(window).height());
+    }
+    else{
+        $('.bubbles-container').css('height',$('.main-container').height());
+    }
+
+    $('.mail-float').mouseover(function () {
+        $('.mail-img').addClass('animated rubberBand');
+    });
+
+    $('.mail-float').mouseleave(function () {
+        $('.mail-img').removeClass('animated rubberBand');
+    });
+});
+
+$(window).on('resize', function(){
+    if($('.main-container').height() < $(window).height()){
+        $('.bubbles-container').css('height',$(window).height());
+    }
+    else{
+        $('.bubbles-container').css('height',$('.main-container').height());
+    }
 
 });
